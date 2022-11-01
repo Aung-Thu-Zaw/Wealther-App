7a9bbb203b8d1f6b35d54bc9562f5fb6e004d6e6
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-11-01 11:09:01 +0630

    refatching data with useAsyncData composable

--- app.vue
+++ app.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="h-screen relative overflow-hidden">
     <img :src="background" />
-    {{ city }}
     <div class="absolute w-full h-full top-0 overlay"></div>
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
@@ -12,7 +11,6 @@
             :src="`https://api.openwealthermap.org/img/wn/${city.wealther[0].icon}@4x.png`"
             class="w-56 icon"
           /> -->
-          <!-- <img :src="city.wealther[0].icon" class="w-56 icon" /> -->
         </div>
         <div>
           <p class="text-9xl text-white font-extralight">
@@ -46,34 +44,45 @@ const background = ref("");
 //     `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
 // );
 
-const { data: city, error } = useAsyncData("city", async () => {
-  const response = await $fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
-  );
+const {
+  data: city,
+  error,
+  // refresh,
+} = useAsyncData(
+  "city",
+  async () => {
+    const response = await $fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
+    );
 
-  const temp = response.main.temp;
+    const temp = response.main.temp;
 
-  if (temp <= -10) {
-    background.value =
-      "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > -10 && temp <= 0) {
-    background.value =
-      "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
-  } else if (temp > 0 && temp <= 10) {
-    background.value =
-      "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
-  } else {
-    background.value =
-      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+    if (temp <= -10) {
+      background.value =
+        "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+    } else if (temp > -10 && temp <= 0) {
+      background.value =
+        "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+    } else if (temp > 0 && temp <= 10) {
+      background.value =
+        "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+    } else {
+      background.value =
+        "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+    }
+    return response;
+  },
+  {
+    watch: [search],
   }
-  return response;
-});
+);
 
 const handleClick = () => {
   const formatedSearch = input.value.trim().split(" ").join("+");
   console.log(formatedSearch);
   search.value = formatedSearch;
   input.value = "";
+  // await refresh();
 };
 </script>
 
