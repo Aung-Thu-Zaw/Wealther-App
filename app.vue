2592ab7e64de5f858a605d94422eacdedd2cae55
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-11-01 10:46:16 +0630

    refatching data with useFetch composable

--- app.vue
+++ app.vue
@@ -1,13 +1,12 @@
 <template>
   <div class="h-screen relative overflow-hidden">
-    {{ search }}
-    {{ error }}
     <img src="" />
+    {{ city }}
     <div class="absolute w-full h-full top-0 overlay"></div>
     <div class="absolute w-full h-full top-0 p-48">
       <div class="flex justify-between">
         <div>
-          <!-- <h1 class="text-7xl text-white">{{ city.name }}</h1> -->
+          <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
           <!-- <img
             :src="`https://openwealthermap.org/img/wn/${city.wealther[0].icon}@4x.png`"
@@ -15,9 +14,9 @@
           /> -->
         </div>
         <div>
-          <!-- <p class="text-9xl text-white font-extralight">
+          <p class="text-9xl text-white font-extralight">
             {{ city.main.temp }}°
-          </p> -->
+          </p>
         </div>
       </div>
       <div class="mt-20">
@@ -26,19 +25,33 @@
           name=""
           class="w-1/2 h-10"
           placeholder="Search a city..."
-          v-model="search"
+          v-model="input"
         />
-        <button class="bg-sky-400 w-20 text-white h-9">Search</button>
+        <button
+          class="bg-sky-400 w-20 text-white h-9"
+          @click="handleClick"
+        >
+          Search
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const search = ref("");
+const search = ref("New York");
+const input = ref("");
 const { data: city, error } = useFetch(
-  `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=a4356e2119114fc98f26c60fd93ade01`
+  () =>
+    `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
 );
+
+const handleClick = () => {
+  const formatedSearch = input.value.trim().split(" ").join("+");
+  console.log(formatedSearch);
+  search.value = formatedSearch;
+  input.value = "";
+};
 </script>
 
 <style scoped>
