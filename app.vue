b9dc7c310ce853fdc8a8b0dcd0f0bf1876b2acb0
Aung-Thu-Zaw <aungthuzaw223@gmail.com>
2022-11-01 11:02:36 +0630

    using useAsyncData composable with fetch wealther api

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="h-screen relative overflow-hidden">
-    <img src="" />
+    <img :src="background" />
     {{ city }}
     <div class="absolute w-full h-full top-0 overlay"></div>
     <div class="absolute w-full h-full top-0 p-48">
@@ -9,9 +9,10 @@
           <h1 class="text-7xl text-white">{{ city.name }}</h1>
           <p class="font-extralight text-2xl mt-2 text-white">Sunday Dec 9th</p>
           <!-- <img
-            :src="`https://openwealthermap.org/img/wn/${city.wealther[0].icon}@4x.png`"
+            :src="`https://api.openwealthermap.org/img/wn/${city.wealther[0].icon}@4x.png`"
             class="w-56 icon"
           /> -->
+          <!-- <img :src="city.wealther[0].icon" class="w-56 icon" /> -->
         </div>
         <div>
           <p class="text-9xl text-white font-extralight">
@@ -27,10 +28,7 @@
           placeholder="Search a city..."
           v-model="input"
         />
-        <button
-          class="bg-sky-400 w-20 text-white h-9"
-          @click="handleClick"
-        >
+        <button class="bg-sky-400 w-20 text-white h-9" @click="handleClick">
           Search
         </button>
       </div>
@@ -41,10 +39,35 @@
 <script setup lang="ts">
 const search = ref("New York");
 const input = ref("");
-const { data: city, error } = useFetch(
-  () =>
+
+const background = ref("");
+// const { data: city, error } = useFetch(
+//   () =>
+//     `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
+// );
+
+const { data: city, error } = useAsyncData("city", async () => {
+  const response = await $fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${search.value}&units=metric&appid=c42bedf884281d7d4e348f97fb1edbdf`
-);
+  );
+
+  const temp = response.main.temp;
+
+  if (temp <= -10) {
+    background.value =
+      "https://images.unsplash.com/photo-1483664852095-d6cc6870702d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+  } else if (temp > -10 && temp <= 0) {
+    background.value =
+      "https://images.unsplash.com/photo-1476820865390-c52aeebb9891?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3540&q=80";
+  } else if (temp > 0 && temp <= 10) {
+    background.value =
+      "https://images.unsplash.com/photo-1560258018-c7db7645254e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=4032&q=80";
+  } else {
+    background.value =
+      "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3546&q=80";
+  }
+  return response;
+});
 
 const handleClick = () => {
   const formatedSearch = input.value.trim().split(" ").join("+");
